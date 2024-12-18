c673a454fd7220f3046e160430359765f9d0f6ef
AlbertoOrlando <albertoorlandowork@gmail.com>
2024-12-18 12:37:41 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// da 1 a 100
+//  frizz 3
+//  buzz 5
+//  fizzbuzz 3 e 5
+
+for(let i= 1; i <= 100; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz");   
+    }
+    else if(i % 5 == 0){
+        console.log("Buzz");
+        
+    }
+    else if(i % 3 == 0 ){
+        console.log("Fizz");
+        
+    }
+    else{
+        console.log(i);
+    }
+}
